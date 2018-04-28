35d4420b5c54f3d365faa80bb5bf0f142cc84325
Warden <zw9409@126.com>
2018-04-28 23:30:10 +0800

    0

--- js/Browser/screen.js
+++
@@ -1,9 +0,0 @@
-console.group(Screen);
-console.log(screen);
-console.log(screen.availHeight);
-console.log(screen.availWidth);
-console.log(screen.colorDepth);
-console.log(screen.pixelDepth);
-console.log(screen.height);
-console.log(screen.width);
-console.groupEnd();
