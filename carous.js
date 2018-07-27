4107d1706306e7ab4c0dcb1221ac9e285235baee
Warden <zw9409@126.com>
2018-07-27 17:27:34 +0800

    1

---
+++ carous.js
@@ -0,0 +1,19 @@
+var ele = document.getElementById('container');
+
+/**
+ * 
+ * @param {Element} el 
+ * @param {Object} opts {pics} 
+ */
+function Carous(el, opts) {
+    this.el = el;
+    this.opts = opts;
+}
+
+/**
+ * 初始化
+ */
+Carous.init = function () {
+    // 轮播图的图片
+    var pics = this.pics;
+};
--- js/extend/Date.js
+++ js/extend/Date.js
@@ -19,24 +19,25 @@ Date.prototype.getDateDifday = function(s1, s2) {
 };
 
 /**
- * 格式化Date对象为 yyyy-mm-dd
- * @param {String} d 连接符，默认为中划线
+ * 格式化Date对象指定格式
+ * @param {String} fmt 格式模板
  */
-Date.prototype.format = function(d) {
-    if (!d) {
-        d = "-";
-    }
-    var oDate = this;
-    var iMonth = oDate.getMonth() + 1;
-    var iDate = oDate.getDate();
-    if (iMonth < 10) {
-        iMonth = "0" + iMonth;
-    }
-    if (iDate < 10) {
-        iDate = "0" + iDate;
-    }
-    var result = oDate.getFullYear() + d + iMonth + d + iDate;
-    return result;
+Date.prototype.format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份   
+        "d+": this.getDate(), //日   
+        "h+": this.getHours(), //小时   
+        "m+": this.getMinutes(), //分   
+        "s+": this.getSeconds(), //秒   
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度   
+        "S": this.getMilliseconds() //毫秒   
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
 };
 
 /**
